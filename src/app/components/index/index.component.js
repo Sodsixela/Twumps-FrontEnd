e986f78d287657d7966625475ee6c53e1664bc1d
Nicolas SAILLY <nsailly@live.fr>
2018-11-08 23:36:58 +0100

    Remove jQuery code and useless functions/variables

--- src/app/components/index/index.component.js
+++ src/app/components/index/index.component.js
@@ -3,15 +3,14 @@
 let index_controller = function indexController($http, $scope, $rootScope, $anchorScroll, $location, $state, GlobalConfigFactory, d3Factory, d3CloudFactory, $element) {
   let self = this;
   self.url = GlobalConfigFactory.url_back;
+  self.collapsed = true
   // Word cloud
   self.tags = [];
   // Emotion
   self.emotion = [];
   // Search functionality
-  self.keyword          = "";
-  self.tweets           = {research: "",data: {}};
-  self.showSearchResult = false;
-  self.count            = 0;
+  self.tweets = {};
+  // Timeline
   self.retweet = [];
   self.timeline = "";
   self.years=[
@@ -32,6 +31,7 @@ let index_controller = function indexController($http, $scope, $rootScope, $anch
   });
 
   $http.get("http://localhost:3005/timeline/").then((response) => {
+    console.log(response.data)
     self.retweet = response.data
     
     
@@ -115,20 +115,10 @@ let index_controller = function indexController($http, $scope, $rootScope, $anch
                     ret.splice(j,1);
                     j--;
                 }
-            };
+            }
             
-        };
+        }
     });
-  });
-
-  $('.btn-expand-collapse').click(function(e) {
-    if ($('input[type="text"][name="keyword"]').val().trim().length > 0) {
-      $('.navbar-primary').removeClass('collapsed');
-    }
-  });
-
-  $('.btn-deflate-collapse').click(function(e) {
-    $('.navbar-primary').addClass('collapsed');
   });
 
   self.scrollTo = function(id) {
@@ -136,46 +126,24 @@ let index_controller = function indexController($http, $scope, $rootScope, $anch
     $anchorScroll();
   }
 
-  $scope.wordClicked = function(word) {
+  self.collapse = () => {
+    self.collapsed = true;
+  }
+
+  self.wordClicked = (keyword) => {
     $http({
       method : 'POST',
       url    : self.url + 'search/',
-      data   : { keyword : word},
+      data   : { keyword : keyword},
       headers: {'Content-Type': 'application/json' }
     }).then((response) => {
       if(response.status === 200) {
-        $('.navbar-primary').removeClass('collapsed');
-
-        self.tweets.research  = word;
-        self.tweets.data      = response.data.data;
-        self.showSearchResult = true;
-        self.count            = response.data.data.length;
-        console.log("Tweets : ", self.tweets.data)
+        self.collapsed = false;
+        self.tweets = response.data.data;
       }
-    })
+    });
   }
 
-}
-  self.submitKeyword = () => {
-    if (self.keyword.trim().length > 0) {
-      $http({
-        method : 'POST',
-        url    : self.url + 'search/',
-        data   : { keyword : self.keyword},
-        headers: {'Content-Type': 'application/json' }
-      }).then((response) => {
-        if(response.status === 200) {
-          $('.navbar-primary').removeClass('collapsed');
-
-          self.tweets.research  = self.keyword;
-          self.keyword          = "";
-          self.tweets.data      = response.data.data;
-          self.showSearchResult = true;
-          self.count            = response.data.data.length;
-        }
-      });
-    }
-  }
 
 };
 
