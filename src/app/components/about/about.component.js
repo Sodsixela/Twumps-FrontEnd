6068725ef82b812d3a3884a8e6f17bce29719dcc
Frédéric ROSSO <frederic.rosso@efrei.net>
2018-11-12 22:10:14 +0100

    Describing database in about

--- src/app/components/about/about.component.js
+++ src/app/components/about/about.component.js
@@ -4,31 +4,7 @@ let about_controller = function aboutController ($http, $state, GlobalConfigFact
   let self = this
   self.api = GlobalConfigFactory.urlBack + 'api/'
   self.url = GlobalConfigFactory.urlBack
-  self.collapsed = true
-  // Search functionality
-  self.tweets = {}
-
-  self.collapse = () => {
-    self.collapsed = true
-  }
-
-  self.wordClicked = (keyword) => {
-    $http({
-      method: 'POST',
-      url: self.api + 'search/',
-      data: {
-        keyword: keyword
-      },
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then((response) => {
-      if (response.status === 200) {
-        self.collapsed = false
-        self.tweets = response.data.data
-      }
-    })
-  }
+  
 }
 
 about_controller.$inject = ['$http', '$state', 'GlobalConfigFactory']
